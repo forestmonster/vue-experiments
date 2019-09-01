105966dde85ceb7a3c148f8a4f4232ea84cc5c89
Forest Monsen <forest.monsen@gmail.com>
2019-09-01 14:29:16 -0700

    Sorting dams by electricity output.
    Shows array sorting in Vue.

--- cue-packt-project/src/App.vue
+++ cue-packt-project/src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="app">
-    <h1>List of expensive experiments</h1>
-    <ul>
-      <li v-bind:key="exp.name" v-for="exp in nonPhysics">
-        {{ exp.name }} ${{ exp.cost }}M USD
-      </li>
-    </ul>
+      <table border=1>
+          <thead>
+              <tr>
+                  <th>Name</th>
+                  <th>Country</th>
+                  <th @click="sort">Electricity</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr v-bind:key="dam.name" v-for="dam in damsByElectricity">
+                  <td>{{ dam.name }}</td>
+                  <td>{{ dam.country }}</td>
+                  <td>{{ dam.electricity }} megawatts</td>
+              </tr>
+          </tbody>
+      </table>
+      <p>Order: {{ order }}</p>
   </div>
 </template>
 
@@ -14,22 +25,33 @@
 export default {
   data() {
     return {
-      experiments: [
-        {name: 'Large Hadron Collider', cost: 650, field: 'Physics'},
-        {name: "Violinist in Metro", cost: 3, field: "Psychology"}
-      ]
+        dams: [
+            {name: "Nurek Dam", country: "Tajikistan", electricity: "3200"},
+            {name: "Three Gorges Dam", country: "China", electricity: "22500"},
+            {name: "Tarbela Dam", country: "Pakistan", electricity: "3500"},
+            {name: "Guri Dam", country: "Venezuela", electricity: "6100"},
+        ],
+        order: 1
     }
   },
 
   computed: {
-    nonPhysics() {
-      return this.experiments
-        .filter(exp => exp.field !== 'Physics')
+    damsByElectricity() {
+      const sortByElectricity = (d1, d2) => (d2.electricity - d1.electricity) * this.order
+
+      return this.dams
+        .slice()
+        .sort(sortByElectricity)
+    }
+  },
+
+  methods: {
+    sort() {
+      this.order = this.order * -1
     }
   }
 }
 </script>
 
 <style>
-
 </style>
