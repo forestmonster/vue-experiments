656a3c2428e21678882103907e3b03ae518c7385
Forest Monsen <forest.monsen@gmail.com>
2019-10-17 12:11:09 -0700

    Simplified computed property of Jon Snow's name.

--- cue-packt-project/src/App.vue
+++ cue-packt-project/src/App.vue
@@ -1,33 +1,28 @@
 <template>
   <div id="app">
     <input type="text" v-model="name"/>
-    <input type="text" id="surname" value="Snow"/>
-    <button @click="saveSurname">Save Surname</button>
+    <input type="text" v-model="surname"/>
+    <br />
+    <p><code>name</code>: <output>{{ name }}</output></p>
+    <p><code>computedFullName</code>: <output>{{ computedFullName }}</output></p>
   </div>
 </template>
 
 <script>
-let surname = 'Snow'
-
 export default {
   data() {
     return {
-      name: "John"
+      name: "John",
+      surname: "Snow"
     }
   },
 
   computed: {
     computedFullName() {
-      return this.name + surname
+      return this.name + this.surname
     }
 
   },
-
-  methods: {
-    saveSurname() {
-      surname = document.querySelector()
-    }
-  }
 }
 </script>
 
