c8cc43c5b70059e20851dd9311799a9ce7e42a08
Forest Monsen <forest.monsen@gmail.com>
2019-11-26 15:12:25 -0800

    Allowing users to enter raw HTML. Ugh!

--- exercises/chapter02/src/outputting_raw_html.vue
+++ exercises/chapter02/src/outputting_raw_html.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="app" v-html="htmlTexts[0]">
+  <div id="app">
+    <input v-model="rawHtml" /><br />
+    <span v-html="rawHtml"></span>
   </div>
 </template>
 
@@ -11,7 +13,8 @@ export default {
         "Dear John,<br />thank you for the <pre>Batman vs Superman</pre> DVD!",
         "Dear John,<br />thank you for the <pre>Ghostbusters 3</pre> DVD!",
         "Dear John,<br />thank you, <b>Gods of Egypt</b> is my new favorite DVD!"
-      ]
+      ],
+      rawHtml: ""
     }
   }
 }
