109546bfc1a5d1babec46c2fbacf4d5a2dabe232
Forest Monsen <forest.monsen@gmail.com>
2019-11-28 21:36:10 -0800

    Call a cab.

---
+++ exercises/chapter03/integrating_with_third_party_css_animation_libraries.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h3>{{ message }}</h3>
+    <button @click="taxiCalled = true">
+      Call a cab
+    </button>
+    <p v-if="taxiCalled">🚖</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hi there",
+      taxiCalled: false
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  font-family: "Lato";
+}
+</style>
+
+<!-- vim:set et nu sw=2 ts=2: -->
