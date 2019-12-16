20ba598081e7933a1ba3081774adc6d9d6aa3c1c
Forest Monsen <forest.monsen@gmail.com>
2019-12-16 14:27:55 -0800

    Add two lists

--- exercises/chapter03-04-05/src/chapter04/making_components_talk_with_vuex.vue
+++ exercises/chapter03-04-05/src/chapter04/making_components_talk_with_vuex.vue
@@ -1,12 +1,53 @@
 <template>
   <div id="app">
+    <div class="wrapper">
+      <input v-model="item" />
+      <button>Add</button>
+
+      <div style="display: flex">
+        <div>
+          <note-card
+          v-for="item in list"
+          v-bind:key="item.id"
+          :remove="() => console.log("removing")"
+          :title="item.name"
+          />
+        </div>
+        <div>
+          <note-summary-list :list="list" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import NoteCard from "./NoteCard.vue"
+import NoteSummaryList from "./NoteSummaryList.vue"
+
 export default {
+  name: "app",
+  components: {
+    NoteCard,
+    NoteSummaryList
+  },
   data() {
     return {
+      item: '',
+      list: [
+        {
+          id: 1,
+          name: 'Dispatch Note 01'
+        },
+        {
+          id: 2,
+          name: 'Dispatch Note 02'
+        },
+        {
+          id: 3,
+          name: 'Dispatch Note 03'
+        }
+      ]
     }
   }
 }
