8c8c5d6df1dee1a536be4a1545cb88fced150a2d
Forest Monsen <forest.monsen@gmail.com>
2019-12-16 14:05:48 -0800

    Add Vuex-based template.

---
+++ exercises/chapter03-04-05/src/chapter04/making_components_talk_with_vuex.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  font-family: "Lato";
+}
+</style>
+
+<!-- vim:set et nu sw=2 ts=2: -->
