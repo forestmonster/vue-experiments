c779bab326310955e7aab191dfba3ae5dcfbb17a
Forest Monsen <forest.monsen@gmail.com>
2020-01-13 14:18:06 -0800

    Enable debugger.

--- exercises/chapter03-04-05/src/chapter05/sending_basic_ajax_requests_with_axios/src/App.vue
+++ exercises/chapter03-04-05/src/chapter05/sending_basic_ajax_requests_with_axios/src/App.vue
@@ -6,19 +6,33 @@
 </template>
 
 <script>
+const btcURL = "https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=EUR"
+
+import axios from "axios"
+
 export default {
-  name: 'app',
+  name: "app",
   data() {
     return {
-      price: 'loading...'
+      price: "loading..."
     }
+  },
+  created() {
+    axios
+      .get(btcURL)
+      .then(x => {
+        this.price = x.data[0].price_usd
+      })
+      .catch(err => {
+        this.price = err.message
+      })
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
