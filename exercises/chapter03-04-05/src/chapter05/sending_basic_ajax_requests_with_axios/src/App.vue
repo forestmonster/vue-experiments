75d25bca91d0efc2b796dad4cb234b0a4a61202e
Forest Monsen <forest.monsen@gmail.com>
2020-01-13 08:30:15 -0800

    Initial commit.

---
+++ exercises/chapter03-04-05/src/chapter05/sending_basic_ajax_requests_with_axios/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h2>Latest Bitcoin price</h2>
+    <p>{{ price }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      price: 'loading...'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
