a5c276dc82ccf884935bdc78142d12c70a867190
Forest Monsen <forest.monsen@gmail.com>
2020-01-15 13:29:24 -0800

    Removing everything to try Axios again.

--- exercises/chapter03-04-05/src/chapter05/validating_user_data_before_sending_it/src/App.vue
+++ exercises/chapter03-04-05/src/chapter05/validating_user_data_before_sending_it/src/App.vue
@@ -1,54 +1,7 @@
 <template>
-  <div id="app">
-    <h3>Write a new post</h3>
-    <form>
-      <div>
-        <label>Title of your post: </label>
-        <input type="text" v-model="title" />
-      </div>
-      <div>
-        <label>Write your thoughts for the day: </label>
-        <textarea name="thoughts" id="thoughts" v-model="body"></textarea>
-      </div>
-      <div>
-        <button @click.prevent="submit">Submit</button>
-      </div>
-    </form>
-    <h3>Response from the server</h3>
-    <pre>{{ response }}</pre>
-  </div>
 </template>
 
 <script>
-import axios from "axios"
-
-export default {
-  data() {
-    return {
-      userId: 1,
-      title: "",
-      body: "",
-      response: "..."
-    }
-  },
-  methods: {
-    submit() {
-      const data = {
-        title: this.title,
-        body: this.body,
-        userId: this.userId
-      }
-
-      const url = "https://jsonplaceholder.typicode.com/posts"
-
-      // eslint-disable-next-line
-      axios.post(url, data).then(x => {
-        // eslint-disable-next-line
-        debugger
-      })
-    }
-  }
-}
 </script>
 
 <style>
