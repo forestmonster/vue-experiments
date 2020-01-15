8e75aec32562e4f036b0c87d14e5612a9039f869
Forest Monsen <forest.monsen@gmail.com>
2020-01-15 10:39:51 -0800

    Add basic form and stub method.

--- exercises/chapter03-04-05/src/chapter05/validating_user_data_before_sending_it/src/App.vue
+++ exercises/chapter03-04-05/src/chapter05/validating_user_data_before_sending_it/src/App.vue
@@ -1,9 +1,29 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <form @submit.prevent="vueSubmit">
+      <div>
+        <label>Name</label>
+        <input type="text" required />
+      </div>
+      <div>
+        <label>E-mail</label>
+        <input type="email" required />
+      </div>
+      <div>
+        <label>Submit</label>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    vueSubmit() {
+    }
+  }
+}
 </script>
 
 <style>
