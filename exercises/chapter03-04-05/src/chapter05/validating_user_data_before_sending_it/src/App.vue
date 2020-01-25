942f11ad2e7fb2a2ca3399cbe24d2a0d1d67e089
Forest Monsen <forest.monsen@gmail.com>
2020-01-24 16:19:37 -0800

    Validating user data.

--- exercises/chapter03-04-05/src/chapter05/validating_user_data_before_sending_it/src/App.vue
+++ exercises/chapter03-04-05/src/chapter05/validating_user_data_before_sending_it/src/App.vue
@@ -1,7 +1,54 @@
 <template>
+  <div id="app">
+    <h3>Create a post</h3>
+    <div>
+      <form @submit.prevent="postSubmit" accept-charset="utf-8">
+        <div>
+          <label>Your name</label>
+          <input type="text" name="name" id="name" required />
+        </div>
+        <div>
+          <label>Your thoughts</label>
+          <input type="text" name="thoughts" id="thoughts" />
+        </div>
+        <div>
+          <button type="submit">Submit thoughts</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios"
+
+export default {
+  data() {
+    return {
+      name: "",
+      thoughts: "",
+      userID: 5,
+      response: ""
+    }
+  },
+  methods: {
+    postSubmit() {
+      const data = {
+        title: this.title,
+        body: this.body,
+        userID: this.userID
+      }
+      const postURL = "https://jsonplaceholder.typicode.com/posts"
+      axios
+        .post(postURL, data)
+        .catch()
+        .then(x => {
+          debugger
+          //this.name = "blah"
+        })
+    }
+  }
+}
 </script>
 
 <style>
@@ -12,5 +59,7 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  padding-bottom: 30px;
+  border: 3px dashed #999;
 }
 </style>
